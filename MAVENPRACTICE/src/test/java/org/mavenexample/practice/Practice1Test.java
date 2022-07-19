53a59af664592be09b7f984f9b373ca2cecc5d18
USER <USER@DESKTOP-R9NCQOU>
2022-07-19 11:43:56 +0530

    1st commit

---
+++ MAVENPRACTICE/src/test/java/org/mavenexample/practice/Practice1Test.java
@@ -0,0 +1,29 @@
+package org.mavenexample.practice;
+
+import org.testng.annotations.Test;
+
+public class Practice1Test {
+	
+@Test
+public void practice1Test() {
+String URL = System.getProperty("url");
+String USERNAME = System.getProperty("username");
+String PASSWORD = System.getProperty("password");
+System.out.println("url"+URL);
+System.out.println("username"+USERNAME);
+System.out.println("password"+PASSWORD);
+System.out.println("Test1===>class1");
+
+}
+
+
+@Test
+public void practice2Test() {
+	System.out.println("Test1===>class1");
+}
+
+@Test
+public void practice3Test() {
+	System.out.println("Test1===>class1");
+}
+}
